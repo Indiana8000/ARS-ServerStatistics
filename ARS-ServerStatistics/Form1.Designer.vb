63aa2114225f622eacf73314cd70f78b19e55f34
Andreas Kreisl <indiana@bluepaw.de>
2020-07-19 15:32:46 +0200

    Projektdateien hinzufügen.

---
+++ ARS-ServerStatistics/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.btn_disconnect = New System.Windows.Forms.Button()
+        Me.btn_Connect = New System.Windows.Forms.Button()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.txt_Password = New System.Windows.Forms.TextBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.txt_Username = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.txt_Port = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.txt_Server = New System.Windows.Forms.TextBox()
+        Me.btn_exec = New System.Windows.Forms.Button()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(881, 24)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 81
+        '
+        'btn_disconnect
+        '
+        Me.btn_disconnect.Enabled = False
+        Me.btn_disconnect.Location = New System.Drawing.Point(779, 24)
+        Me.btn_disconnect.Name = "btn_disconnect"
+        Me.btn_disconnect.Size = New System.Drawing.Size(75, 20)
+        Me.btn_disconnect.TabIndex = 79
+        Me.btn_disconnect.Text = "Logout"
+        Me.btn_disconnect.UseVisualStyleBackColor = True
+        '
+        'btn_Connect
+        '
+        Me.btn_Connect.Location = New System.Drawing.Point(698, 24)
+        Me.btn_Connect.Name = "btn_Connect"
+        Me.btn_Connect.Size = New System.Drawing.Size(75, 20)
+        Me.btn_Connect.TabIndex = 78
+        Me.btn_Connect.Text = "Login"
+        Me.btn_Connect.UseVisualStyleBackColor = True
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(494, 8)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(53, 13)
+        Me.Label17.TabIndex = 77
+        Me.Label17.Text = "Password"
+        '
+        'txt_Password
+        '
+        Me.txt_Password.Location = New System.Drawing.Point(492, 24)
+        Me.txt_Password.Name = "txt_Password"
+        Me.txt_Password.Size = New System.Drawing.Size(200, 20)
+        Me.txt_Password.TabIndex = 76
+        Me.txt_Password.UseSystemPasswordChar = True
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(288, 8)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(55, 13)
+        Me.Label16.TabIndex = 75
+        Me.Label16.Text = "Username"
+        '
+        'txt_Username
+        '
+        Me.txt_Username.Location = New System.Drawing.Point(286, 24)
+        Me.txt_Username.Name = "txt_Username"
+        Me.txt_Username.Size = New System.Drawing.Size(200, 20)
+        Me.txt_Username.TabIndex = 74
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(222, 8)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(26, 13)
+        Me.Label15.TabIndex = 73
+        Me.Label15.Text = "Port"
+        '
+        'txt_Port
+        '
+        Me.txt_Port.Location = New System.Drawing.Point(220, 24)
+        Me.txt_Port.Name = "txt_Port"
+        Me.txt_Port.Size = New System.Drawing.Size(60, 20)
+        Me.txt_Port.TabIndex = 72
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(16, 8)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(38, 13)
+        Me.Label14.TabIndex = 71
+        Me.Label14.Text = "Server"
+        '
+        'txt_Server
+        '
+        Me.txt_Server.Location = New System.Drawing.Point(14, 24)
+        Me.txt_Server.Name = "txt_Server"
+        Me.txt_Server.Size = New System.Drawing.Size(200, 20)
+        Me.txt_Server.TabIndex = 70
+        '
+        'btn_exec
+        '
+        Me.btn_exec.Enabled = False
+        Me.btn_exec.Location = New System.Drawing.Point(1087, 24)
+        Me.btn_exec.Name = "btn_exec"
+        Me.btn_exec.Size = New System.Drawing.Size(75, 20)
+        Me.btn_exec.TabIndex = 82
+        Me.btn_exec.Text = "Run"
+        Me.btn_exec.UseVisualStyleBackColor = True
+        '
+        'Chart1
+        '
+        ChartArea1.AxisY.Minimum = 0R
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Me.Chart1.Dock = System.Windows.Forms.DockStyle.Fill
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(0, 0)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(1174, 549)
+        Me.Chart1.TabIndex = 83
+        Me.Chart1.Text = "Chart1"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.IsSplitterFixed = True
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txt_Server)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.btn_exec)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label14)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.DateTimePicker1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txt_Port)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.btn_disconnect)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label15)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.btn_Connect)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txt_Username)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label17)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label16)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txt_Password)
+        Me.SplitContainer1.Panel1MinSize = 48
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Chart1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1174, 601)
+        Me.SplitContainer1.SplitterDistance = 48
+        Me.SplitContainer1.TabIndex = 84
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1174, 601)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.MinimumSize = New System.Drawing.Size(1190, 320)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents btn_disconnect As Button
+    Friend WithEvents btn_Connect As Button
+    Friend WithEvents Label17 As Label
+    Friend WithEvents txt_Password As TextBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents txt_Username As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents txt_Port As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents txt_Server As TextBox
+    Friend WithEvents btn_exec As Button
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents SplitContainer1 As SplitContainer
+End Class
---
+++ ARS-ServerStatistics/Form1.vb
@@ -0,0 +1,152 @@
+﻿Public Class Form1
+    Dim oServer As BMC.ARSystem.Server
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Date.Now.Year > 2020 Or Date.Now.Month > 9 Then
+            MsgBox("Trial Period Expired! Please contact the Developer.", vbExclamation, "Unregistered Version")
+            Application.Exit()
+        End If
+
+        oServer = New BMC.ARSystem.Server
+
+        txt_Server.Text = My.Settings.Server
+        txt_Port.Text = My.Settings.Port
+        txt_Username.Text = My.Settings.Username
+        If txt_Server.Text.Length > 0 Then
+            txt_Password.Select()
+        End If
+    End Sub
+
+    Private Sub btn_Connect_Click(sender As Object, e As EventArgs) Handles btn_Connect.Click
+        Dim alForms As ArrayList
+
+        My.Settings.Server = txt_Server.Text
+        My.Settings.Port = txt_Port.Text
+        My.Settings.Username = txt_Username.Text
+        My.Settings.Save()
+
+        Try
+            oServer.Login(txt_Server.Text, txt_Username.Text, txt_Password.Text, "", "de_DE", "UTF-8", txt_Port.Text)
+            alForms = oServer.GetListForm()
+            btn_Connect.Enabled = False
+            btn_disconnect.Enabled = True
+            btn_exec.Enabled = True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btn_disconnect_Click(sender As Object, e As EventArgs) Handles btn_disconnect.Click
+        oServer.Logout()
+        btn_Connect.Enabled = True
+        btn_disconnect.Enabled = False
+        btn_exec.Enabled = False
+    End Sub
+
+    Private Sub txt_Password_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_Password.KeyPress
+        If txt_Password.Text.Length > 0 And Asc(e.KeyChar) = 13 Then
+            btn_Connect.Select()
+            Application.DoEvents()
+            btn_Connect_Click(sender, New EventArgs())
+        End If
+    End Sub
+
+    Private Sub btn_exec_Click(sender As Object, e As EventArgs) Handles btn_exec.Click
+        btn_exec.Enabled = False
+        Application.DoEvents()
+        Dim xColor() As Color = {Color.Red, Color.Blue, Color.Green, Color.Pink, Color.Purple, Color.DarkGray}
+
+        Dim requestFields As BMC.ARSystem.EntryListFieldList = New BMC.ARSystem.EntryListFieldList
+        requestFields.Add(New BMC.ARSystem.EntryListField(3)) 'Time
+        requestFields.Add(New BMC.ARSystem.EntryListField(913)) 'API Proc Time
+        requestFields.Add(New BMC.ARSystem.EntryListField(940)) 'Filter Proc Time
+        requestFields.Add(New BMC.ARSystem.EntryListField(953)) 'DB SQL Proc Time
+        requestFields.Add(New BMC.ARSystem.EntryListField(951)) 'ARServer Proc Time
+        'requestFields.Add(New BMC.ARSystem.EntryListField(958)) 'Network Time
+        requestFields.Add(New BMC.ARSystem.EntryListField(905)) 'User Count
+
+        For i = 0 To requestFields.Count - 1
+            If Chart1.Series.Count > i Then
+                Chart1.Series("Series" & i).Points.Clear()
+            Else
+                Chart1.Series.Add("Series" & i)
+                Chart1.Series("Series" & i).ChartType = DataVisualization.Charting.SeriesChartType.Line
+                Chart1.Series("Series" & i).Color = xColor(i)
+                Chart1.Series("Series" & i).BorderWidth = 3
+            End If
+        Next
+
+
+        Dim row As BMC.ARSystem.EntryFieldValue
+        Dim result As BMC.ARSystem.EntryFieldValueList
+        Dim last() As Integer = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+        Dim xDate As DateTime
+        Dim xPos As Integer
+        Dim uTime As Double
+        uTime = (DateTimePicker1.Value.Date - New Date(1970, 1, 1, 0, 0, 0)).TotalSeconds
+        Console.WriteLine("'3' >= " & uTime & " AND '3' < " & (uTime + 86400))
+        result = oServer.GetListEntryWithFields("Server Statistics", "'3' >= " & uTime & " AND '3' < " & (uTime + 86400 * 7), requestFields, 0, 1000)
+
+
+        Chart1.ChartAreas(0).AxisX.Interval = 1
+        Chart1.ChartAreas(0).AxisX.LabelStyle.Angle = -90
+        'Chart1.ChartAreas(0).AxisY.Maximum = 1000000
+
+
+
+        Console.WriteLine("Count: " & result.Count)
+        For Each row In result
+            For i = 0 To requestFields.Count - 1
+                If row.FieldValues.Keys(i) = 3 Then
+                    xPos = i
+                ElseIf row.FieldValues.Keys(i) = 905 Then
+                    Chart1.Series("Series" & i).YAxisType = DataVisualization.Charting.AxisType.Secondary
+                End If
+            Next
+
+            'xDate = New DateTime(1970, 1, 1, 0, 0, 0)
+            'xDate = xDate.AddSeconds(row.FieldValues.Values(xPos))
+            For i = 0 To requestFields.Count - 1
+                If last(i) > 0 Then
+                    If row.FieldValues.Values(i) - last(i) > 0 Then
+                        'Chart1.Series("Series" & i).Points.Add(row.FieldValues.Values(i) - last(i))
+                        If row.FieldValues.Keys(i) = 905 Then
+                            Chart1.Series("Series" & i).Points.AddXY(row.FieldValues.Values(xPos), row.FieldValues.Values(i))
+                        Else
+                            Chart1.Series("Series" & i).Points.AddXY(row.FieldValues.Values(xPos), row.FieldValues.Values(i) - last(i))
+                        End If
+                    Else
+                        'Chart1.Series("Series" & i).Points.Add(0)
+                    End If
+                Else
+
+                    If row.FieldValues.Keys(i) = 913 Then
+                        Chart1.Series("Series" & i).LegendText = "API"
+                    ElseIf row.FieldValues.Keys(i) = 940 Then
+                        Chart1.Series("Series" & i).LegendText = "FILTER"
+                    ElseIf row.FieldValues.Keys(i) = 953 Then
+                        Chart1.Series("Series" & i).LegendText = "SQL"
+                    ElseIf row.FieldValues.Keys(i) = 951 Then
+                        Chart1.Series("Series" & i).LegendText = "ARServer"
+                    ElseIf row.FieldValues.Keys(i) = 958 Then
+                        Chart1.Series("Series" & i).LegendText = "Network"
+                    ElseIf row.FieldValues.Keys(i) = 905 Then
+                        Chart1.Series("Series" & i).LegendText = "User"
+                    Else
+                        Chart1.Series("Series" & i).LegendText = row.FieldValues.Keys(i)
+                    End If
+
+
+
+
+                End If
+                If i <> xPos Then last(i) = row.FieldValues.Values(i)
+            Next
+        Next
+
+        btn_exec.Enabled = True
+    End Sub
+
+    Private Sub Chart1_Click(sender As Object, e As EventArgs) Handles Chart1.Click
+
+    End Sub
+End Class
